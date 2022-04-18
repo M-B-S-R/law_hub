5a5ef4164c1c6cfb6efe7b1edf179d240bccda6d
Ahmad Naeem <ahmadnaeem26831@gmail.com>
2022-04-18 10:52:19 -0700

    propose kiya hai

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Love you jan',
+      title: 'i love u 2',
       theme: ThemeData(
 
         primarySwatch: Colors.deepOrange,
